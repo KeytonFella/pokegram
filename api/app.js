18a5483ddb2c443d8e6efc1922c60244bc0c564a
Peter-Desktop <pn7000@gmail.com>
2023-10-03 17:02:51 -0500

    Created basic pokemon team dao

--- api/app.js
+++ api/app.js
@@ -2,13 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 app.use(cors());
-
+app.use(bodyParser.json())
 app.get('/', (req, res) => {
     res.send('Hello World!');
     }
 );
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
