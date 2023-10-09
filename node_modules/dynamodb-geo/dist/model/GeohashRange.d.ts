63989d477773da20da9bd9d481b3fa5a9378f2dc
arinaihara <aiharaarin@gmail.com>
2023-10-09 09:30:33 -0700

    reinstall node packages

---
+++ node_modules/dynamodb-geo/dist/model/GeohashRange.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="long" />
+import * as Long from 'long';
+export declare class GeohashRange {
+    rangeMin: Long;
+    rangeMax: Long;
+    constructor(min: Long, max: Long);
+    tryMerge(range: GeohashRange): boolean;
+    trySplit(hashKeyLength: any): GeohashRange[];
+}
