316825d06b3e21aff3596c1276ab246ec4336e61
Keyton Friske <keytonfriske@gmail.com>
2023-10-09 18:21:00 -0500

    updated file name of controller and import

--- app.js
+++ app.js
@@ -4,14 +4,12 @@ const port = 3000;
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const profileRouter = require('./controller/profileController');
-
-
-const tradesRouter = require('./controller/trades');
-app.use('/trades', tradesRouter);
+const tradesRouter = require('./controller/tradesController');
 
 app.use(cors());
 app.use(bodyParser.json())
 app.use('/api/profiles', profileRouter);
+app.use('/trades', tradesRouter);
 
 app.get('/', (req, res) => {
     res.send('Hello and welcome to the Pokegram API!');
