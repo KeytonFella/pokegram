3205be091e27483412d479dcc450f901c91396fb
Peter <pn7000@gmail.com>
2023-10-16 17:24:17 -0500

    pulled from main

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const profileRouter = require('./controller/profileController');
 const tradesRouter = require('./controller/tradesController');
 const registerRouter = require('./controller/registerController');
 const loginRouter = require('./controller/loginController');
+const addressesRouter = require('./controller/addressesController');
 const logger = require('./utility/middleware/logger');
 const jwt = require('./utility/jwt_util');
 const jwks = require('./utility/jwks_util');
@@ -35,6 +36,7 @@ app.get("/protected", (req, res) => {
 
 app.use('/api/profiles', profileRouter);
 app.use('/api/trades', tradesRouter);
+app.use('/api/addresses', addressesRouter);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
