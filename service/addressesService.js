61ba4a9964d3c3ad01e413093612f853a953fe9f
arinaihara <aiharaarin@gmail.com>
2023-10-18 12:56:05 -0700

    git update

--- service/addressesService.js
+++ service/addressesService.js
@@ -2,26 +2,26 @@ const addressesDAO = require('../repository/addressesDAO');
 const axios = require('axios');
 const dotenv = require('dotenv');
 dotenv.config();
-const { SecretsManagerClient, GetSecretValueCommand} = require("@aws-sdk/client-secrets-manager");
+// const { SecretsManagerClient, GetSecretValueCommand} = require("@aws-sdk/client-secrets-manager");
   
-const secret_name = "prod/GG/key";
-const client = new SecretsManagerClient({region: "us-east-2"});
-let response;
-try {
-    response = client.send(
-      new GetSecretValueCommand({
-        SecretId: secret_name,
-        VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
-      })
-      ).then((data) => {
-            response = data;
-        }).catch((err) => { console.log(err) });
-      console.log(response)
-  } catch (error) {
-      throw error;
-  }
-const API_KEY = response.SecretString;
-// const API_KEY = process.env.API_KEY;
+// const secret_name = "prod/GG/key";
+// const client = new SecretsManagerClient({region: "us-east-2"});
+// let response;
+// try {
+//     response = client.send(
+//       new GetSecretValueCommand({
+//         SecretId: secret_name,
+//         VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+//       })
+//       ).then((data) => {
+//             response = data;
+//         }).catch((err) => { console.log(err) });
+//       console.log(response)
+//   } catch (error) {
+//       throw error;
+//   }
+// const API_KEY = response.SecretString;
+const API_KEY = process.env.API_KEY;
 const GOOGLE_MAPS_API = 'https://maps.googleapis.com/maps/api/geocode/json?address='
 
 // ============================== Geocoding Service Calls ==============================
