5ec8643428a311c7832782cd1d457e1c033b73cd
Peter <pn7000@gmail.com>
2023-10-19 15:27:06 -0500

    Working on teamservice test

--- __tests__/teamService.test.js
+++ __tests__/teamService.test.js
@@ -6,14 +6,17 @@ jest.mock('../repository/teamDAO.js');
 
 describe('Create Team Service', () => {
     it('should validate and create a new team for a user', async() => {
-        const result = await service.createTeam("team", [
+        const testTeam = {'teamName': "team", 'pokemonList': [
             {pokemonName: "Pikachu", level: 10, nickname: "JohnDoe"},
             {pokemonName: "Vulpix", level: 12},
             {pokemonName: "Oshawott", level: 11},
             {pokemonName: "Wailord", level: 100},
             {pokemonName: "Electabuzz", level: 34},
-            {pokemonName: "Arbok", level: 70}])
-        expect(result).toEqual({teamName: 'team'})
+            {pokemonName: "Arbok", level: 70}]
+        }
+        const createTeam = jest.fn().mockResolvedvalue(testTeam) 
+        const result = await service.createTeam(testTeam.teamName, testTeam.pokemonList)
+        expect(result).toEqual(testTeam)
     })
 
     it('should handle empty team name', async () => {
@@ -21,15 +24,6 @@ describe('Create Team Service', () => {
             {pokemonName: "Scizor", level: 30}])
         expect(result).toBeNull()
     });
-
-    // it('should be able to handle errors', async () =>{
-    //     dao.createTeam.mockRejectedValue(new Error('DAO error'));
-
-    //     const result = await service.createTeam('team', [
-    //         {pokemonName: "Ariados", level: 31},
-    //         {pokemonName: "Metagross", level: 45}]);
-    //     expect(result).toBeNull()
-    // }) 
 
     it('should reject an empty pokemon list', async () => {
         const result = await service.createTeam('newteam', [])
