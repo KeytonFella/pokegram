a7c057ded4ee7a59db8d4e1ddcacad9d089722ef
Peter <pn7000@gmail.com>
2023-10-19 15:53:12 -0500

    committing changes to team service test before attempting to refactor validation

--- __tests__/teamService.test.js
+++ __tests__/teamService.test.js
@@ -1,6 +1,7 @@
 const service = require('../service/teamService.js')
 const dao = require('../repository/teamDAO.js')
-const mw = require('../utility/middleware/teamMW')
+const mw = require('../utility/middleware/teamMW');
+const { afterEach } = require('node:test');
 
 jest.mock('../repository/teamDAO.js');
 
@@ -14,19 +15,33 @@ describe('Create Team Service', () => {
             {pokemonName: "Electabuzz", level: 34},
             {pokemonName: "Arbok", level: 70}]
         }
-        const createTeam = jest.fn().mockResolvedvalue(testTeam) 
+        dao.createTeam = jest.fn().mockResolvedValue(testTeam) 
         const result = await service.createTeam(testTeam.teamName, testTeam.pokemonList)
         expect(result).toEqual(testTeam)
     })
 
     it('should handle empty team name', async () => {
-        const result = await service.createTeam('', [
-            {pokemonName: "Scizor", level: 30}])
+        const noName = {'teamName': '', 'pokemonList': [
+            {pokemonName: "Pikachu", level: 10, nickname: "JohnDoe"},
+            {pokemonName: "Vulpix", level: 12},
+            {pokemonName: "Oshawott", level: 11},
+            {pokemonName: "Wailord", level: 100},
+            {pokemonName: "Electabuzz", level: 34},
+            {pokemonName: "Arbok", level: 70}
+        ]}
+        service.createTeam = jest.fn().mockResolvedValue(noName)
+        const result = await service.createTeam(noName)
         expect(result).toBeNull()
     });
 
     it('should reject an empty pokemon list', async () => {
-        const result = await service.createTeam('newteam', [])
+        const noTeam = {'teamName': "newteam", 'pokemonList': []}
+        service.createTeam = jest.fn().mockResolvedValue(noTeam)
+        const result = await service.createTeam(noTeam)
         expect(result).toBeNull()
+    })
+
+    afterEach(() => {
+        jest.clearAllMocks()
     })
 })
