f947ed7e847fca7ada243f02ac6c6d1d227c0a01
Peter <pn7000@gmail.com>
2023-10-21 11:45:21 -0500

    TeamService testing

--- __tests__/teamMW.test.js
+++ __tests__/teamMW.test.js
@@ -1,17 +1,23 @@
-const {mw, validateTeam} = require('../utility/middleware/teamMW');
+const mw = require('../utility/middleware/teamMW');
 
-jest.mock('../utility/middleware/teamMW', () => {
-    const mw = jest.requireActual('../utility/middleware/teamMW.js')
+// jest.mock('../utility/middleware/teamMW', () => {
+//     const mw = jest.requireActual('../utility/middleware/teamMW.js')
 
-    return {
-        __esModule: true,
-        ...mw,
-        default: jest.fn(() => 'mocked mw'),
+//     return {
+//         __esModule: true,
+//         ...mw,
+//         default: jest.fn(() => 'mocked mw'),
 
-    }
-})
+//     }
+// })
+
+mockMW = jest.fn()
 
 describe("Team validation", () => {
+    beforeAll(() => {
+        mockMW.mockClear()
+    })
+
     const valid = {
         teamName: 'test', pokemonList: [
         {pokemonName: 'Pikachu', level: 3},
@@ -19,14 +25,7 @@ describe("Team validation", () => {
         {pokemonName: 'Swellow', level: 19}]
     }
 
-    const noname = {teamName: '', pokemonList: [
-        {pokemonName: "Pikachu", level: 10, nickname: "JohnDoe"},
-        {pokemonName: "Vulpix", level: 12},
-        {pokemonName: "Oshawott", level: 11},
-        {pokemonName: "Wailord", level: 100},
-        {pokemonName: "Electabuzz", level: 34},
-        {pokemonName: "Arbok", level: 70}
-    ]}
+    
     const mockReq = (body) => {
         return {
             body: body
@@ -42,28 +41,19 @@ describe("Team validation", () => {
         return res;
     }
 
+    
+
     it('should return true for a valid team', async() => {
         const mockedNext = jest.fn();
         const mockedReq = mockReq(valid);
         const mockedRes = mockRes()
         //const expectedRes = {statusCode: 200, message: 'Team submitted: [object Object]'}
-        await validateTeam(mockedReq, mockedRes, mockedNext)
+        await mw.validateTeam(mockedReq, mockedRes, mockedNext)
         expect(mockedReq.body.valid).toEqual(true)
         expect(mockedNext.mock.calls.length).toBe(1);
         //expect(mockedRes.status).toHaveBeenCalledWith(200)
         //expect(mockedRes.body).toHaveBeenCalledWith(req.body)
     })
 
-    it('should return false for an empty team name', async() => {
-        const mockedNext = jest.fn();
-        const mockedReq = mockReq(noname);
-        const mockedRes = mockRes()
-        //const expectedRes = {statusCode: 200, message: 'Team submitted: [object Object]'}
-        await mw.validateTeam(mockedReq, mockedRes, mockedNext)
-        expect(mockedReq.body.valid).toEqual(false)
-        // expect(mockedRes.status).toHaveBeenCalledWith(400)
-        // expect(mockedRes.body).toHaveBeenCalledWith({message: "Error: Team is missing a name"})
-        
-        expect(mockedNext.mock.calls.length).toBe(1);
-    })
+    
 })
--- __tests__/teamService.test.js
+++ __tests__/teamService.test.js
@@ -4,24 +4,46 @@ const { afterEach } = require('node:test')
 
 jest.mock('../repository/teamDAO.js');
 
-describe('Create Team Service', () => {
+describe('Team Service', () => {
+    // afterEach(() => {
+    //     jest.clearAllMocks()
+    // })
+    const uuid = '32423afd-ec3c-4a37-a163-137c99e99173'
+    const updateTeam = {teamName: "updatedteam", pokemonList: [
+        {pokemonName: "Pikachu", level: 10, nickname: "JohnDoe"},
+        {pokemonName: "Vulpix", level: 12},
+        {pokemonName: "Oshawott", level: 100},
+        {pokemonName: "Wailord", level: 100},
+        {pokemonName: "Electabuzz", level: 34},
+        {pokemonName: "Arbok", level: 70}],
+        user_id: uuid
+    }
+    const testTeam = {teamName: "team", pokemonList: [
+        {pokemonName: "Pikachu", level: 10, nickname: "JohnDoe"},
+        {pokemonName: "Vulpix", level: 12},
+        {pokemonName: "Oshawott", level: 11},
+        {pokemonName: "Wailord", level: 100},
+        {pokemonName: "Electabuzz", level: 34},
+        {pokemonName: "Arbok", level: 70}],
+        user_id: uuid
+    }
     it('should validate and create a new team for a user', async() => {
-        const testTeam = {'teamName': "team", 'pokemonList': [
-            {pokemonName: "Pikachu", level: 10, nickname: "JohnDoe"},
-            {pokemonName: "Vulpix", level: 12},
-            {pokemonName: "Oshawott", level: 11},
-            {pokemonName: "Wailord", level: 100},
-            {pokemonName: "Electabuzz", level: 34},
-            {pokemonName: "Arbok", level: 70}]
-        }
+        
         dao.createTeam = jest.fn().mockResolvedValue(testTeam) 
-        const result = await service.createTeam(testTeam.teamName, testTeam.pokemonList)
+        const result = await service.createTeam(testTeam.teamName, testTeam.pokemonList, uuid)
         expect(result).toEqual(testTeam)
     })
 
+    it('should get team by user id', async() => {
+        dao.getTeamByUserId = jest.fn().mockResolvedValue(testTeam)
+        const result = await service.getTeamByUserId(uuid)
+        expect(result).toEqual(testTeam)
+    })
 
-    afterEach(() => {
-        jest.clearAllMocks()
+    it('should validate and update a team', async() => {
+        dao.updateTeamById = jest.fn().mockResolvedValue(updateTeam)
+        const result = await service.updateTeamById(uuid, updateTeam.teamName, updateTeam.pokemonList)
+        expect(result).toEqual(updateTeam)
     })
 })
 
