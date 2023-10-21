7413e2ad179ff5cdb8fc24f43da0046dd14d7493
Peter <pn7000@gmail.com>
2023-10-21 09:09:02 -0500

    teamMW test tweaks

--- __tests__/teamMW.test.js
+++ __tests__/teamMW.test.js
@@ -1,18 +1,69 @@
-const mw = require('../utility/middleware/teamMW');
+const {mw, validateTeam} = require('../utility/middleware/teamMW');
 
-jest.mock('mw')
+jest.mock('../utility/middleware/teamMW', () => {
+    const mw = jest.requireActual('../utility/middleware/teamMW.js')
 
-// test("shouldmake sure pokemon names are valid", async () => {
-//     const response = await mw.validatePokemonNames([
-//         {pokemonName: "Pikachu", level: 2},
-//         {pokemonName: "Electrike", level: 5},
-//         {pokemonName: "Poliwag", level: 3}
-//     ])
+    return {
+        __esModule: true,
+        ...mw,
+        default: jest.fn(() => 'mocked mw'),
+
+    }
+})
+
+describe("Team validation", () => {
+    const valid = {
+        teamName: 'test', pokemonList: [
+        {pokemonName: 'Pikachu', level: 3},
+        {pokemonName: 'Tentacool', level: 14},
+        {pokemonName: 'Swellow', level: 19}]
+    }
+
+    const noname = {teamName: '', pokemonList: [
+        {pokemonName: "Pikachu", level: 10, nickname: "JohnDoe"},
+        {pokemonName: "Vulpix", level: 12},
+        {pokemonName: "Oshawott", level: 11},
+        {pokemonName: "Wailord", level: 100},
+        {pokemonName: "Electabuzz", level: 34},
+        {pokemonName: "Arbok", level: 70}
+    ]}
+    const mockReq = (body) => {
+        return {
+            body: body
+            
+        }
+    }
+
+    const mockRes = () => {
+        const res = {};
     
-//     expect(response).toEqual(true)
-// })
+        
+        res.body = jest.fn().mockReturnValue(res)
+        return res;
+    }
 
-const mockReq = () => {
-    const req = {body: {'teamName': 'test', 'pokemonList': [{'pokemonName': 'Pikahu'}]}}
-    return req
-}
+    it('should return true for a valid team', async() => {
+        const mockedNext = jest.fn();
+        const mockedReq = mockReq(valid);
+        const mockedRes = mockRes()
+        //const expectedRes = {statusCode: 200, message: 'Team submitted: [object Object]'}
+        await validateTeam(mockedReq, mockedRes, mockedNext)
+        expect(mockedReq.body.valid).toEqual(true)
+        expect(mockedNext.mock.calls.length).toBe(1);
+        //expect(mockedRes.status).toHaveBeenCalledWith(200)
+        //expect(mockedRes.body).toHaveBeenCalledWith(req.body)
+    })
+
+    it('should return false for an empty team name', async() => {
+        const mockedNext = jest.fn();
+        const mockedReq = mockReq(noname);
+        const mockedRes = mockRes()
+        //const expectedRes = {statusCode: 200, message: 'Team submitted: [object Object]'}
+        await mw.validateTeam(mockedReq, mockedRes, mockedNext)
+        expect(mockedReq.body.valid).toEqual(false)
+        // expect(mockedRes.status).toHaveBeenCalledWith(400)
+        // expect(mockedRes.body).toHaveBeenCalledWith({message: "Error: Team is missing a name"})
+        
+        expect(mockedNext.mock.calls.length).toBe(1);
+    })
+})
--- __tests__/teamService.test.js
+++ __tests__/teamService.test.js
@@ -1,6 +1,5 @@
 const service = require('../service/teamService.js')
 const dao = require('../repository/teamDAO.js')
-const mw = require('../utility/middleware/teamMW');
 const { afterEach } = require('node:test')
 
 jest.mock('../repository/teamDAO.js');
@@ -20,24 +19,6 @@ describe('Create Team Service', () => {
         expect(result).toEqual(testTeam)
     })
 
-    it('should handle empty team name', async () => {
-        const noName = {'teamName': '', 'pokemonList': [
-            {pokemonName: "Pikachu", level: 10, nickname: "JohnDoe"},
-            {pokemonName: "Vulpix", level: 12},
-            {pokemonName: "Oshawott", level: 11},
-            {pokemonName: "Wailord", level: 100},
-            {pokemonName: "Electabuzz", level: 34},
-            {pokemonName: "Arbok", level: 70}
-        ]}
-        
-    });
-
-    it('should reject an empty pokemon list', async () => {
-        const noTeam = {'teamName': "newteam", 'pokemonList': []}
-        service.createTeam = jest.fn().mockResolvedValue(noTeam)
-        const result = await service.createTeam(noTeam)
-        expect(result).toBeNull()
-    })
 
     afterEach(() => {
         jest.clearAllMocks()
